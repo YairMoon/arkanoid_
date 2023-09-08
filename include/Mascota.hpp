5b1e01259022a487cd378b21ed5dc337d727d492
YairMoon <143459675+YairMoon@users.noreply.github.com>
2023-09-08 22:54:07 +0000

    se agregaron las clases del juego

---
+++ include/Mascota.hpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+class Mascota
+{
+private:
+    /* data */
+public:
+    Mascota(/* args */) {}
+    ~Mascota() {}
+    void Comer(){
+        std::cout<<"int:"<<sizeof(int)<<std::endl;
+        std::cout<<"char:"<<sizeof(char)<<std::endl;
+        std::cout<<"bool:"<<sizeof(bool)<<std::endl;
+        std::cout<<
+        std::cout<<
+        std::cout<<
+        
+
+    }
+    
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
+#include <Mascota.hpp>
+
 int main(int argc, char const *argv[])
 {
-    int a = 0;
-    for (int contador = 0; contador < 10; contador++)
-    {
-        a += 1
-    }
-
+     std::cout << "Inicio del programa" << std::endl;
+    //Mascota m1;
+    //Mascota m2;
+    //m1.Comer();
+   // m2.Comer();
+   
     return 0;
 }
